4061745ca4e47274642beed1891c8d8b148ba0d7
Divyank Shah <divyank.shah.2016@gmail.com>
2022-07-09 02:42:08 -0700

    deploy fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    basePath: "/nextjs-pages",
+    assetPrefix: "/nextjs-pages"
 }
 
 module.exports = nextConfig
